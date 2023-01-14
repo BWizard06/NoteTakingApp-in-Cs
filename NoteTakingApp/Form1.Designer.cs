98f0bb3d16ab4272c9500a1bc521784ae700b821
BWizard06 <117916203+homeparty-me@users.noreply.github.com>
2023-01-14 15:41:20 +0100

    New Design

--- NoteTakingApp/Form1.Designer.cs
+++ NoteTakingApp/Form1.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.titleBox = new System.Windows.Forms.TextBox();
             this.noteBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -42,23 +46,31 @@
             // 
             // titleBox
             // 
-            this.titleBox.Location = new System.Drawing.Point(710, 74);
+            this.titleBox.BackColor = System.Drawing.Color.Black;
+            this.titleBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.titleBox.ForeColor = System.Drawing.Color.Red;
+            this.titleBox.Location = new System.Drawing.Point(710, 53);
             this.titleBox.Name = "titleBox";
             this.titleBox.Size = new System.Drawing.Size(734, 39);
             this.titleBox.TabIndex = 0;
             // 
             // noteBox
             // 
-            this.noteBox.Location = new System.Drawing.Point(710, 166);
+            this.noteBox.BackColor = System.Drawing.Color.Black;
+            this.noteBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.noteBox.ForeColor = System.Drawing.Color.Red;
+            this.noteBox.Location = new System.Drawing.Point(710, 146);
             this.noteBox.Multiline = true;
             this.noteBox.Name = "noteBox";
-            this.noteBox.Size = new System.Drawing.Size(734, 691);
+            this.noteBox.Size = new System.Drawing.Size(734, 711);
             this.noteBox.TabIndex = 1;
             // 
             // label1
             // 
+            this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(710, 33);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(710, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(734, 38);
             this.label1.TabIndex = 2;
@@ -68,7 +80,8 @@
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(710, 125);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(710, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(734, 38);
             this.label3.TabIndex = 4;
@@ -77,11 +90,43 @@
             // 
             // previousNotes
             // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Red;
+            this.previousNotes.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.previousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.previousNotes.BackgroundColor = System.Drawing.Color.White;
+            this.previousNotes.BackgroundColor = System.Drawing.Color.Black;
+            this.previousNotes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.previousNotes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.previousNotes.DefaultCellStyle = dataGridViewCellStyle3;
+            this.previousNotes.GridColor = System.Drawing.Color.DimGray;
             this.previousNotes.Location = new System.Drawing.Point(12, 12);
             this.previousNotes.Name = "previousNotes";
+            this.previousNotes.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Red;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.previousNotes.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.previousNotes.RowHeadersVisible = false;
             this.previousNotes.RowHeadersWidth = 82;
             this.previousNotes.RowTemplate.Height = 41;
             this.previousNotes.Size = new System.Drawing.Size(692, 662);
@@ -90,8 +135,10 @@
             // 
             // loadButton
             // 
-            this.loadButton.BackColor = System.Drawing.Color.White;
+            this.loadButton.BackColor = System.Drawing.Color.Black;
+            this.loadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.loadButton.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.loadButton.ForeColor = System.Drawing.Color.Red;
             this.loadButton.Location = new System.Drawing.Point(12, 680);
             this.loadButton.Name = "loadButton";
             this.loadButton.Size = new System.Drawing.Size(346, 90);
@@ -102,8 +149,11 @@
             // 
             // deleteButton
             // 
-            this.deleteButton.BackColor = System.Drawing.Color.White;
+            this.deleteButton.BackColor = System.Drawing.Color.Black;
+            this.deleteButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.deleteButton.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.deleteButton.ForeColor = System.Drawing.Color.Red;
             this.deleteButton.Location = new System.Drawing.Point(364, 680);
             this.deleteButton.Name = "deleteButton";
             this.deleteButton.Size = new System.Drawing.Size(340, 90);
@@ -114,8 +164,10 @@
             // 
             // saveButton
             // 
-            this.saveButton.BackColor = System.Drawing.Color.White;
+            this.saveButton.BackColor = System.Drawing.Color.Black;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.saveButton.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.saveButton.ForeColor = System.Drawing.Color.Red;
             this.saveButton.Location = new System.Drawing.Point(364, 776);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(340, 81);
@@ -126,8 +178,10 @@
             // 
             // newNoteButton
             // 
-            this.newNoteButton.BackColor = System.Drawing.Color.White;
+            this.newNoteButton.BackColor = System.Drawing.Color.Black;
+            this.newNoteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.newNoteButton.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.newNoteButton.ForeColor = System.Drawing.Color.Red;
             this.newNoteButton.Location = new System.Drawing.Point(12, 776);
             this.newNoteButton.Name = "newNoteButton";
             this.newNoteButton.Size = new System.Drawing.Size(346, 81);
@@ -140,7 +194,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Cyan;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1466, 869);
             this.Controls.Add(this.newNoteButton);
             this.Controls.Add(this.saveButton);
